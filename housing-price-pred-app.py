93fd8468593ecde6aee5477bf82f1694eacd1235
Jubair Ahmed Nabin <49593276+nabin47@users.noreply.github.com>
2021-12-10 23:33:09 +0600

    Uncomment line 18

--- housing-price-pred-app.py
+++ housing-price-pred-app.py
@@ -15,7 +15,7 @@ from sklearn.model_selection import train_test_split
 train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
 import numpy as np
-# housing['income_cat'] = pd.cut(housing['median_income'], bins=[0., 1.5, 3.0, 4.5, 6., np.inf], labels=[1, 2, 3, 4, 5])
+housing['income_cat'] = pd.cut(housing['median_income'], bins=[0., 1.5, 3.0, 4.5, 6., np.inf], labels=[1, 2, 3, 4, 5])
 # housing['income_cat'].hist()
 # plt.show()
 
