c25008a8c7371843b1521e5b6ed0662642a77c21
Jubair Ahmed Nabin <49593276+nabin47@users.noreply.github.com>
2021-12-10 23:28:45 +0600

    Comment all print and graphs

--- housing-price-pred-app.py
+++ housing-price-pred-app.py
@@ -1,42 +1,42 @@
 import pandas as pd
 DATA_URL=('https://raw.githubusercontent.com/ageron/handson-ml/master/datasets/housing/housing.csv')
 housing = pd.read_csv(DATA_URL)
-housing.head()
+# housing.head()
 
-housing.info()
+# housing.info()
 
-housing.ocean_proximity.value_counts()
+# housing.ocean_proximity.value_counts()
 
-import matplotlib.pyplot as plt
-housing.hist(bins=50, figsize=(10, 8))
-plt.show()
+# import matplotlib.pyplot as plt
+# housing.hist(bins=50, figsize=(10, 8))
+# plt.show()
 
 from sklearn.model_selection import train_test_split
 train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
 import numpy as np
-housing['income_cat'] = pd.cut(housing['median_income'], bins=[0., 1.5, 3.0, 4.5, 6., np.inf], labels=[1, 2, 3, 4, 5])
-housing['income_cat'].hist()
-plt.show()
+# housing['income_cat'] = pd.cut(housing['median_income'], bins=[0., 1.5, 3.0, 4.5, 6., np.inf], labels=[1, 2, 3, 4, 5])
+# housing['income_cat'].hist()
+# plt.show()
 
 from sklearn.model_selection import StratifiedShuffleSplit
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(housing, housing["income_cat"]):
     strat_train_set = housing.loc[train_index]
     strat_test_set = housing.loc[test_index]
-print(strat_test_set['income_cat'].value_counts() / len(strat_test_set))
+# print(strat_test_set['income_cat'].value_counts() / len(strat_test_set))
 
 for set_ in (strat_train_set, strat_test_set):
     set_.drop('income_cat', axis=1, inplace=True)
 housing = strat_train_set.copy()
 
-housing.plot(kind='scatter', x='longitude', y='latitude', alpha=0.4, s=housing['population']/100, label='population',
-figsize=(12, 8), c='median_house_value', cmap=plt.get_cmap('jet'), colorbar=True)
-plt.legend()
-plt.show()
+# housing.plot(kind='scatter', x='longitude', y='latitude', alpha=0.4, s=housing['population']/100, label='population',
+# figsize=(12, 8), c='median_house_value', cmap=plt.get_cmap('jet'), colorbar=True)
+# plt.legend()
+# plt.show()
 
 corr_matrix = housing.corr()
-print(corr_matrix.median_house_value.sort_values(ascending=False))
+# print(corr_matrix.median_house_value.sort_values(ascending=False))
 
 pd.plotting.scatter_matrix(housing.iloc[:, 4:36])
 
@@ -45,7 +45,7 @@ housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
 housing["population_per_household"] = housing["population"]/housing["households"]
 
 corr_matrix = housing.corr()
-print(corr_matrix["median_house_value"].sort_values(ascending=False))
+# print(corr_matrix["median_house_value"].sort_values(ascending=False))
 
 # Data Preparation
 housing = strat_train_set.drop("median_house_value", axis=1)
@@ -103,7 +103,7 @@ lin_reg.fit(housing_prepared, housing_labels)
 data = housing.iloc[:5]
 labels = housing_labels.iloc[:5]
 data_preparation = full_pipeline.transform(data)
-print("Predictions: ", lin_reg.predict(data_preparation))
+# print("Predictions: ", lin_reg.predict(data_preparation))
 
 import streamlit as st
 
